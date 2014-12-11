6abbe34b638a8fcaed61bbcdea302a4c632b5d00
Robert Podwika Mac <robertpodwikamac@MacBook-Pro-Robert.local>
2014-12-11 23:43:31 +0100

    init commit

---
+++ app/views/layout/base.html%%e%%.php
@@ -0,0 +1,11 @@
+a:5:{i:0;s:78:"<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>";s:5:"title";a:1:{i:0;a:4:{s:4:"type";i:357;s:5:"value";s:25:"Default title of the page";s:4:"file";s:29:"../app/views/layout/base.html";s:4:"line";i:5;}}i:1;s:28:"</title>
+</head>
+<body>
+    ";s:4:"body";a:1:{i:0;a:4:{s:4:"type";i:357;s:5:"value";s:5:"
+    ";s:4:"file";s:29:"../app/views/layout/base.html";s:4:"line";i:9;}}i:2;s:16:"
+</body>
+</html>";}
---
+++ public/index.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: robertpodwikamac
+ * Date: 11.12.14
+ * Time: 22:41
+ */
+error_reporting(E_ALL);
+
+try {
+
+    //Register an autoloader
+    $loader = new \Phalcon\Loader();
+    $loader->registerDirs(array(
+        '../app/controllers/',
+        '../app/models/'
+    ))->register();
+
+    //Create a DI
+    $di = new Phalcon\DI\FactoryDefault();
+
+    //Setup the view component
+    $di->set('view', function(){
+        $view = new \Phalcon\Mvc\View();
+        $view->setViewsDir('../app/views/');
+        $view->registerEngines([
+            ".html" =>  'Phalcon\Mvc\View\Engine\Volt'
+        ]);
+        return $view;
+    });
+
+    //Setup a base URI so that all generated URIs include the "tutorial" folder
+    $di->set('url', function(){
+        $url = new \Phalcon\Mvc\Url();
+        $url->setBaseUri('/');
+        return $url;
+    });
+
+    //Handle the request
+    $application = new \Phalcon\Mvc\Application($di);
+
+    echo $application->handle()->getContent();
+
+} catch(\Phalcon\Exception $e) {
+    echo "PhalconException: ", $e->getMessage();
+}
